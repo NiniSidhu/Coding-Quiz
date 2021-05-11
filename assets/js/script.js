3abeafe5073425211dbdb054674837c3a2c670b8
NayanSidhu <nsidh068@uottawa.ca>
2021-05-11 15:40:18 -0400

    start js for hs

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var timeLeft = document.querySelector("#timeLeft");
+var startTime = document.querySelector("#startTime");
+var questions = document.querySelector("#questions");
+var wrapper = document.querySelector("#wrapper");
+var score = 0;
+var questionsIndex = 0; 
+
+//Time and the deductions 
